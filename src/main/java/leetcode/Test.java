70c69cdd212d186aeef42965c4cc4e4f744407da
Blank-1058 <liukang7464@163.com>
2019-03-25 22:46:06 +0800

    leetcode

--- src/main/java/leetcode/Test.java
+++ src/main/java/leetcode/Test.java
@@ -3,17 +3,27 @@ package leetcode;
 public class Test {
 
     public static void main(String[] arg0){
-        MinStack minStack=new MinStack();
-        minStack.push(-2);
-        minStack.push(0);
-        minStack.push(-3);
-        int min=minStack.getMin();
-        System.out.println(min);
-        minStack.pop();
-        int top=minStack.top();
-        System.out.println(top);
-        min=minStack.getMin();
-        System.out.println(min);
+//        MinStack minStack=new MinStack();
+//        minStack.push(-2);
+//        minStack.push(0);
+//        minStack.push(-3);
+//        int min=minStack.getMin();
+//        System.out.println(min);
+//        minStack.pop();
+//        int top=minStack.top();
+//        System.out.println(top);
+//        min=minStack.getMin();
+//        System.out.println(min);
 
+
+        LRUCache obj = new LRUCache(2);
+        obj.put(2,1);
+        obj.put(3,2);
+        int value=obj.get(3);
+        value=obj.get(2);
+        obj.put(4,3);
+        value=obj.get(2);
+        value=obj.get(3);
+        value=obj.get(4);
     }
 }
