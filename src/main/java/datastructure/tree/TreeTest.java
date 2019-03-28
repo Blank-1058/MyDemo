cf5d2f1bbdbc40f798ba2aecb9f9dd3cabe46243
刘康491X <liukang_lc@inspur.com>
2019-03-28 17:31:39 +0800

    leetcode

--- src/main/java/datastructure/tree/TreeTest.java
+++ src/main/java/datastructure/tree/TreeTest.java
@@ -2,6 +2,8 @@ package datastructure.tree;
 
 import datastructure.tree.demo.TreeTraverse;
 
+import java.util.List;
+
 /**
  * 测试类
  *
@@ -13,6 +15,19 @@ public class TreeTest {
         int[] nums=new int[]{1,2,3,4,5,6,7,8,9};
         BinaryTreeNode root=TreeNodeUtil.createTree(nums);
 //        TreeTraverse.preorderTraverse(root);
-        TreeTraverse.inorderTraverse(root);
+        List<Integer> result=TreeTraverse.preorderTraverseRecursion(root);
+        TreeTraverse.printList("************递归前序遍历**************",result);
+        result.clear();
+        result=TreeTraverse.preorderTraverseNotRecursion(root);
+        TreeTraverse.printList("************非递归前序遍历**************",result);
+        result.clear();
+        result=TreeTraverse.inorderTraverseRecursion(root);
+        TreeTraverse.printList("************递归中序遍历**************",result);
+        result.clear();
+        result=TreeTraverse.inorderTraverseNotRecursion(root);
+        TreeTraverse.printList("************非递归中序遍历**************",result);
+        result.clear();
+        result=TreeTraverse.postorderTraverseRecursion(root);
+        TreeTraverse.printList("************递归后序遍历**************",result);
     }
 }
--- src/main/java/datastructure/tree/demo/TreeTraverse.java
+++ src/main/java/datastructure/tree/demo/TreeTraverse.java
@@ -2,6 +2,8 @@ package datastructure.tree.demo;
 
 import datastructure.tree.BinaryTreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -15,48 +17,44 @@ public class TreeTraverse {
      * 前序遍历
      * @param root
      */
-    public static void preorderTraverse(BinaryTreeNode root){
+    public static List<Integer> preorderTraverseRecursion(BinaryTreeNode root){
+        List<Integer> result=new ArrayList<>();
         if(root==null){
             System.out.println("tree is null");
+            return result;
         }
-        System.out.println("****前序遍历递归实现****");
-        System.out.print("[");
-        preorderTraverseRecursion(root);
-        System.out.print("]");
-        System.out.println();
-
-        System.out.println("****前序遍历非递归实现****");
-        System.out.print("[");
-        preorderTraverseNotRecursion(root);
-        System.out.print("]");
-        System.out.println();
-
-
+        preorderTraverseRecursion(root,result);
+        return result;
     }
 
     /**
      * 用递归方式实现前序遍历
      * @param node
      */
-    private static void preorderTraverseRecursion(BinaryTreeNode node){
+    private static void preorderTraverseRecursion(BinaryTreeNode node,List<Integer> result){
         if(node==null){
             return;
         }
-        System.out.print(node.getVal()+",");
-        preorderTraverseRecursion(node.getLeftNode());
-        preorderTraverseRecursion(node.getRightNode());
+        result.add(node.getVal());
+        preorderTraverseRecursion(node.getLeftNode(),result);
+        preorderTraverseRecursion(node.getRightNode(),result);
     }
 
     /**
      * 非递归方式实现前序遍历
-     * @param node
+     * @param root
      */
-    private static void preorderTraverseNotRecursion(BinaryTreeNode node){
+    public static List<Integer> preorderTraverseNotRecursion(BinaryTreeNode root){
+        List<Integer> result=new ArrayList<>();
+        if(root==null){
+            System.out.println("tree is null");
+            return result;
+        }
         Stack<BinaryTreeNode> stack=new Stack<>();
-        stack.push(node);
+        stack.push(root);
         while(!stack.isEmpty()){
             BinaryTreeNode parent=stack.pop();
-            System.out.print(parent.getVal()+",");
+            result.add(parent.getVal());
             if(parent.getRightNode()!=null){
                 stack.push(parent.getRightNode());
             }
@@ -64,47 +62,119 @@ public class TreeTraverse {
                 stack.push(parent.getLeftNode());
             }
         }
+        return result;
     }
 
     /**
      * 中序遍历
      * @param root
      */
-    public static void inorderTraverse(BinaryTreeNode root){
+    public static List<Integer> inorderTraverseRecursion(BinaryTreeNode root){
+        List<Integer> result=new ArrayList<>();
         if(root==null){
-            System.out.println("tree is null");
+            return result;
         }
-        System.out.println("****中序遍历递归实现****");
-        System.out.print("[");
-        inorderTraverseRecursion(root);
-        System.out.print("]");
-        System.out.println();
-
-//        System.out.println("****中序遍历非递归实现****");
-//        System.out.print("[");
-//        inorderTraverseNotRecursion(root);
-//        System.out.print("]");
-//        System.out.println();
+        inorderTraverseRecursion(root,result);
+        return result;
     }
 
     /**
      * 用递归方式实现中序遍历
      * @param node
      */
-    private static void inorderTraverseRecursion(BinaryTreeNode node){
+    private static void inorderTraverseRecursion(BinaryTreeNode node,List<Integer> result){
         if(node==null){
             return;
         }
-        inorderTraverseRecursion(node.getLeftNode());
-        System.out.print(node.getVal()+",");
-        inorderTraverseRecursion(node.getRightNode());
+        inorderTraverseRecursion(node.getLeftNode(),result);
+        result.add(node.getVal());
+        inorderTraverseRecursion(node.getRightNode(),result);
     }
 
     /**
      * 用非递归方式实现中序遍历
+     * @param root
+     */
+    public static List<Integer> inorderTraverseNotRecursion(BinaryTreeNode root){
+        List<Integer> result=new ArrayList<>();
+        Stack<BinaryTreeNode> stack=new Stack<>();
+        BinaryTreeNode tmp=root;
+        while(!stack.empty() || tmp!=null){
+            while(tmp!=null){
+                stack.push(tmp);
+                tmp=tmp.getLeftNode();
+            }
+            if(!stack.empty()){
+                //当某个节点的左子节点为空时，使该节点出栈，然后访问此节点的右子节点
+                tmp=stack.pop();
+                result.add(tmp.getVal());
+                tmp=tmp.getRightNode();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 用递归方式实现后序遍历
+     * @param root
+     * @return
+     */
+    public static List<Integer> postorderTraverseRecursion(BinaryTreeNode root){
+        List<Integer> result=new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        postorderTraverseRecursion(root,result);
+        return result;
+    }
+
+    /**
+     * 用递归方式实现后序遍历
      * @param node
      */
-    private static void inorderTraverseNotRecursion(BinaryTreeNode node){
+    private static void postorderTraverseRecursion(BinaryTreeNode node,List<Integer> result){
+        if(node==null){
+            return;
+        }
+        postorderTraverseRecursion(node.getLeftNode(),result);
+        postorderTraverseRecursion(node.getRightNode(),result);
+        result.add(node.getVal());
+    }
 
+    /**
+     * 用非递归方式实现后序遍历
+     * @param root
+     */
+    public static List<Integer> postorderTraverseNotRecursion(BinaryTreeNode root){
+        List<Integer> result=new ArrayList<>();
+        Stack<BinaryTreeNode> stack=new Stack<>();
+        //当前访问的节点
+        BinaryTreeNode curNode=root;
+        //上一次访问的节点
+        BinaryTreeNode lastNode=null;
+        while(!stack.empty()){
+            while(curNode!=null){
+                stack.push(curNode);
+                lastNode=curNode;
+                curNode=curNode.getLeftNode();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 打印list结果
+     * @param message
+     * @param list
+     */
+    public static void printList(String message,List<Integer> list){
+        System.out.println(message);
+        StringBuilder printStr=new StringBuilder("[");
+        for(int i:list){
+            printStr.append(i).append(",");
+        }
+        printStr.deleteCharAt(printStr.length()-1);
+        printStr.append("]");
+        System.out.println(printStr);
     }
 }
