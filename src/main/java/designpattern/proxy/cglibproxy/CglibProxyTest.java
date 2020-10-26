4578e4fd3542aa66d56c5a23abb143ad5b2bfa09
刘康491X <liukang_lc@inspur.com>
2020-10-26 19:28:41 +0800

    cglib代理模式修改

--- src/main/java/designpattern/proxy/cglibproxy/CglibProxyTest.java
+++ src/main/java/designpattern/proxy/cglibproxy/CglibProxyTest.java
@@ -1,5 +1,7 @@
 package designpattern.proxy.cglibproxy;
 
+import net.sf.cglib.proxy.Enhancer;
+
 /**
  * cglib动态代理测试
  *
@@ -7,8 +9,16 @@ package designpattern.proxy.cglibproxy;
  */
 public class CglibProxyTest {
     public static void main(String[] args) {
+        // 在UserProxy类中添加了getProxy方法的情况下使用这种调用方式
         UserProxy proxy=new UserProxy();
         MyUser myUser= (MyUser) proxy.getProxy(MyUser.class);
         myUser.login("admin");
+
+        // 在UserProxy类中没有添加getProxy方法的情况下使用这种调用方式
+        Enhancer enhancer=new Enhancer();
+        enhancer.setSuperclass(MyUser.class);
+        enhancer.setCallback(new UserProxy());
+        MyUser myUser1 = (MyUser) enhancer.create();
+        myUser1.login("test");
     }
 }
